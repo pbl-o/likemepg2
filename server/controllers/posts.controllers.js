bf80a496835ca1530b58c46a9b64ee124863c1b2
pblo <pdiazarenas541@gmail.com>
2026-02-01 13:28:31 +0000

    FEAT/ edit controllers.js

---
+++ server/controllers/posts.controllers.js
@@ -0,0 +1,134 @@
+import postModel from "../models/posts.model.js";
+
+const read = async (req,res) =>{
+try {
+    const posts = await postModel.getAll()
+    if(!posts){
+        return res.status(404).json({message: "Posts not found"})
+    }
+    return res.status(200).json(posts)
+} catch (error) {
+    return res.status(500).json({message: "Internal server error"})
+    
+}
+}
+const readById= async (req,res) =>{
+try {
+    const post = await postModel.getById()
+    if(!post){
+        return res.status(404).json({message: "Post not found"})
+    }
+    return res.status(200).json(post)
+} catch (error) {
+    return res.status(500).json({message: "Internal server error"})
+    
+}
+}
+const create = async (req,res) =>{
+    const post = req.body
+try {
+    const post = await postModel.agregar()
+    if(!post){
+        return res.status(404).json({message: "Post not found"})
+    }
+    return res.status(200).json(post)
+} catch (error) {
+    return res.status(500).json({message: "Internal server error"})
+    
+}
+}
+
+const updateLikes = async (req,res) =>{
+    const {id} = req.params
+try {
+    const post = await postModel.modificarLikes()
+    if(!post){
+        return res.status(404).json({message: "Post not found"})
+    }
+    return res.status(200).json(post)
+} catch (error) {
+    return res.status(500).json({message: "Internal server error"})
+    
+}
+}
+
+const remove = async (req,res) =>{
+
+    const {id} = req.params
+
+try {
+    const post = await postModel.eliminar()
+    if(!post){
+        return res.status(404).json({message: "Post not found"})
+    }
+    return res.status(200).json(post)
+} catch (error) {
+    return res.status(500).json({message: "Internal server error"})
+    
+}
+}
+//via req.query (único)
+const updateSingle = async (req,res) =>{
+    const {id} = req.params
+    const campo = Object.keys(req.query)[0]
+    const valor = req.query[campo] 
+try {
+    const post = await postModel.modificarSingle(campo, valor, id)
+    if(!post){
+        return res.status(404).json({message: "Post not found"})
+    }
+    return res.status(200).json(post)
+} catch (error) {
+    return res.status(500).json({message: "Internal server error"})
+    
+}
+}
+
+//via req.query (flexible)
+const updateMulti = async (req,res) =>{
+    const {id} = req.params
+    const modificados = req.query
+try {
+    const post = await postModel.modificarMulti(modificados, id)
+    if(!post){
+        return res.status(404).json({message: "Post not found"})
+    }
+    return res.status(200).json(post)
+} catch (error) {
+    return res.status(500).json({message: "Internal server error"})
+    
+}
+}
+//via req.body
+const updateAll = async (req,res) =>{
+    const {id} =req.params
+    const {titulo, img, descripcion, likes} = req.body
+
+try {
+    const post = await postModel.modificarVarios(
+        titulo,
+        img,
+        descripcion,
+        likes,
+        id
+    )
+    if(!post){
+        return res.status(404).json({message: "Post not found"})
+    }
+    return res.status(200).json(posts)
+} catch (error) {
+    return res.status(500).json({message: "Internal server error"})
+    
+}
+}
+
+const postController = {
+    read,
+    readById,
+    create,
+    updateLikes,
+    updateSingle,
+    updateMulti,
+    updateAll,
+    remove
+}
